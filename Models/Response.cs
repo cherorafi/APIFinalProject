8efb4b25b8f887990744726b4b7aa619b763aaa1
cherorafi <80270966+cherorafi@users.noreply.github.com>
2022-12-05 01:10:54 -0500

    Add files via upload

---
+++ Models/Response.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Models
+{
+    public class Response
+    {
+        public int statusCode { get; set; } 
+        public string statusDescription { get; set; }
+        public object? recieved { get; set; }
+        
+    }
+}
